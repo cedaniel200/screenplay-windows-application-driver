6f3260c04778d4897befb144a5f730c3e4b0e561
cesar.meneses <cesar.meneses@ceiba.com.co>
2018-09-03 09:35:16 -0500

    The OnStage class is used for scenario management

--- src/test/java/com/cedaniel200/screenplay/standalone/calculator/stepdefinitions/AddTwoNumbersStepDefinitions.java
+++ src/test/java/com/cedaniel200/screenplay/standalone/calculator/stepdefinitions/AddTwoNumbersStepDefinitions.java
@@ -4,35 +4,31 @@ import com.cedaniel200.screenplay.standalone.calculator.questions.TheResult;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import net.serenitybdd.screenplay.Actor;
-import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
-import net.thucydides.core.annotations.Managed;
-import org.openqa.selenium.WebDriver;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
 
 import static com.cedaniel200.screenplay.standalone.calculator.model.NumberToOperateBuilder.theNumbers;
 import static com.cedaniel200.screenplay.standalone.calculator.tasks.Add.add;
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 import static org.hamcrest.core.Is.is;
 
 public class AddTwoNumbersStepDefinitions {
 
-    @Managed
-    private WebDriver driver;
-    private Actor cesar = Actor.named("Cesar");
-
     @Before
     public void prepareStage() {
-        cesar.can(BrowseTheWeb.with(driver));
+        OnStage.setTheStage(new OnlineCast());
     }
 
     @When("^I add (\\d+) and (\\d+)$")
-    public void addTwoNumbers(int firstNumber, int secondNumber) throws Exception {
-        cesar.attemptsTo(add(theNumbers(firstNumber).and(secondNumber)));
+    public void addTwoNumbers(int firstNumber, int secondNumber) {
+        theActorCalled("Cesar").attemptsTo(add(theNumbers(firstNumber).and(secondNumber)));
     }
 
     @Then("^the result should be (\\d+)$")
-    public void theResultShouldBe(int thisNumber) throws Exception {
-        cesar.should(seeThat(TheResult.ofTheSum(), is(thisNumber)));
+    public void theResultShouldBe(int thisNumber) {
+        theActorInTheSpotlight().should(seeThat(TheResult.ofTheSum(), is(thisNumber)));
     }
 
 }
