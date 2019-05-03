728a58e72ebf53af546b530b2ab615dae2d880ce
cesar daniel <cdanielmg200@gmail.com>
2019-05-03 11:02:25 -0500

    Se mejora la legibilidad del  código

--- src/main/java/com/cedaniel200/screenplay/standalone/calculator/tasks/Add.java
+++ src/main/java/com/cedaniel200/screenplay/standalone/calculator/tasks/Add.java
@@ -4,13 +4,14 @@ import com.cedaniel200.screenplay.standalone.calculator.model.NumberToOperate;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.conditions.Check;
 
 import java.util.Iterator;
 
 import static com.cedaniel200.screenplay.standalone.calculator.userinterfaces.CalculatorPage.*;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
 
-public class Add implements Task{
+public class Add implements Task {
 
     private NumberToOperate numbersToOperate;
 
@@ -18,19 +19,20 @@ public class Add implements Task{
         this.numbersToOperate = numbersToOperate;
     }
 
-    public static Add add(NumberToOperate numbersToOperate){
+    public static Add add(NumberToOperate numbersToOperate) {
         return instrumented(Add.class, numbersToOperate);
     }
 
     @Override
     public <T extends Actor> void performAs(T actor) {
         Iterator<Integer> numbers = numbersToOperate.getNumbers().iterator();
-        while (numbers.hasNext()){
-            actor.attemptsTo( Click.on(NUMERIC_BUTTON.of(Integer.toString(numbers.next()))));
-            if(numbers.hasNext()){
-                actor.attemptsTo( Click.on(ADD_BUTTON) );
-            }
+        while (numbers.hasNext()) {
+            String number = numbers.next().toString();
+            actor.attemptsTo(
+                    Click.on(NUMERIC_BUTTON.of(number)),
+                    Check.whether(numbers.hasNext()).andIfSo(Click.on(ADD_BUTTON))
+            );
         }
-        actor.attemptsTo( Click.on(EQUAL_BUTTON) );
+        actor.attemptsTo(Click.on(EQUAL_BUTTON));
     }
 }
